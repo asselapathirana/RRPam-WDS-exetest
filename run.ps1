88065ce2a5bbc2e684a6895e45dbcc6d4f0241b6
Assela Pathirana <assela@pathirana.net>
2016-11-29 12:25:42 +0100

    testing with new strategy. No sikuli

--- run.ps1
+++ run.ps1
@@ -1,4 +1,4 @@
 
 $env:PATH="C:\\projects\\rrpam-wds-exetest\\rrpamwds\\{app}" 
-$MyProcess = Start-Process $env:PATH\rrpamwds.exe -PassThru
+$MyProcess = Start-Process $env:PATH\rrpamwds.exe "dummy" -PassThru
 Start-Sleep -s 30 
--- script.sikuli/script.py
+++
@@ -1,9 +0,0 @@
-find("1480339253994.png")
-for n in range(10):
-    click("1480339271093.png")
-    wait("Capture.PNG")
-    click("Capture.PNG")
-
-click("1480340297592.png")
-type("\n")
-
