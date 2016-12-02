0c8c2f7c0a64d4cdb735f6e1e56406dd70f3c5a4
Assela Pathirana <assela@pathirana.net>
2016-12-02 15:41:42 +0100

    powershell syntax error

--- run.ps1
+++ run.ps1
@@ -1,5 +1,5 @@
 
 $env:PATH="$($pwd)\\rrpamwds\\{app}"  
-$MyProcess = Start-Process $($env:PATH)\rrpamwds.exe -PassThru -ArgumentList "ga"
+$MyProcess = Start-Process $env:PATH\rrpamwds.exe -PassThru -ArgumentList "ga"
 & .\screenshot.ps1
 
